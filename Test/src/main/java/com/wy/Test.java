83d4871a07d9f82f9014179553a6b75ae180667d
chuxiawy <chuxiawy@yeah.net>
2022-04-06 20:25:31 +0800

    这个是用分支上传的代码哦~！

--- Test/src/main/java/com/wy/Test.java
+++
@@ -1,8 +0,0 @@
-package com.wy;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("第一次使用git");
-        System.out.println("第二次使用啦");
-    }
-}
---
+++ Test/src/main/java/com/wy/Test2.java
@@ -0,0 +1,7 @@
+package com.wy;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("这是我的分支操作");
+    }
+}
